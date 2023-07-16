d0b629ff88984db3caf076ffb873926308538dba
rhit-LEHMANCB <cblehman22@gmail.com>
2023-07-16 18:29:35 -0400

    add basic auth0 functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import { connectToDatabase } from "./services/database.service"
 import { usersRouter } from "./routes/users.router";
+import { auth, requiresAuth } from "express-openid-connect";
 import config from "./config/config";
 import * as path from "path";
+
+export const authConfig = {
+    authRequired: true,
+    auth0Logout: true,
+    secret: config.AUTH_SECRET,
+    baseURL: config.AUTH_BASE_URL,
+    clientID: config.AUTH_CLIENT_ID,
+    issuerBaseURL: config.AUTH_ISSUER_BASE_URL
+};
 
 const app = express();
 
@@ -12,6 +22,11 @@ connectToDatabase()
     .then(() => {
         app.use(express.static(path.join(__dirname, "../src/web/build")));
         app.use("/api/users", usersRouter);
+        app.use(auth(authConfig));
+
+        app.get('/profile', (req, res) => {
+            res.send(JSON.stringify(req.oidc.user));
+        });
 
         app.get("*", (req, res) => {
             res.sendFile(
